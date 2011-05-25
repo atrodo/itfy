3f24fa2db146cbd9aed635938c8f00f83a915cfe
Jon Gentle <atrodo@atrodo.org>
2011-05-25 07:59:58 -0400

    Rewrite itfy-run to use the remote API

--- itfy-run/itfy-run.pl
+++ itfy-run/itfy-run.pl
@@ -9,51 +9,87 @@ use local::lib "$FindBin::Bin/../perl5";
 use lib "$FindBin::Bin/../../tool_bench/lib/";
 
 use autodie qw/:all/;
-use itfy::Schema::ItfyDB;
+use File::Temp;
+use JSON::Any;
+use LWP::UserAgent;
+
 require Tool::Bench;
 
-my $schema = itfy::Schema::ItfyDB->connect("dbi:SQLite:dbname=$FindBin::Bin/../itfy.sqlite");
+# Build projects at revisions
+# Run each bench_cmd
 
-my $root = "/home/atrodo/parrot/parrot";
-my $machine = $schema->resultset("Machine")->find({name => "ipfy"});
-my $project = $schema->resultset("Project")->find({name => "Parrot"});
+# Load Config
+my $config = do "$FindBin::Bin/config.pl";
 
-foreach my $rev (@ARGV)
+my $root = "$FindBin::Bin";
+
+# Loop (possibly) forever
+while (1)
 {
-  chdir $root;
 
-  warn "Building...\n";
-  system("bash build-it.sh $rev 1>/dev/null 2>/dev/null");
+  # Connect to website w/key to download tasks
+  my $browser  = LWP::UserAgent->new;
+  my $host     = $config->{host};
+  my $key      = $config->{api_key};
+  my $response = $browser->get("$host/rpc/v1/$key/todo");
 
-  my $git_log = qx/git log $rev -1 --format=%H:%ct:%cD/;
-  chomp $git_log;
+  die "Can't get it -- ", $response->status_line
+      unless $response->is_success;
 
-  my ($rev_hash, $rev_stamp, $rev_date) = split m/:/, $git_log, 3;
+  my $json_raw = $response->decoded_content;
 
-  my $git_describe = qx/git describe $rev --tags/;
-  chomp $git_describe;
+  my $json = JSON::Any->jsonToObj($json_raw);
 
-  warn "Running commands...\n";
-  foreach my $cmd ($project->bench_cmds->all)
+  foreach my $todo ( @{ $json->{list} } )
   {
-    warn "  T::B-ing " . $cmd->name . "...\n";
-    my $bench= Tool::Bench->new;
-    my $exec = join ' ', $cmd->interp, $cmd->cmd, $cmd->args;
-    $bench->add_items( $cmd->name => sub{qx{$exec}});
-    $bench->run($cmd->count);
-    my $json = $bench->report(
-      format => "JSON",
-      interp => $cmd->interp,
-    );
+    my $tempdir     = File::Temp->newdir();
+    my $install_dir = $tempdir->dirname;
 
-    $cmd->add_result_json({
-      revision => $rev_hash,
-      revision_aka => $git_describe,
-      revision_date => $rev_date,
-      revision_stamp => $rev_stamp,
-      machine => $machine,
-      json => $json,
-    });
+    local $ENV{PATH} = "$install_dir/bin:$ENV{PATH}";
+    warn $ENV{PATH};
+
+    my $rev;
+
+    my $build_it = sub
+    {
+      my $project = shift;
+      chdir $config->{build_root};
+
+      if ( !-d $project->{git_name} )
+      {
+        system( "git clone " . $project->{url} . " " . $project->{git_name} );
+      }
+
+      chdir $project->{git_name};
+
+      # Checkout
+      system("git fetch");
+      system("git clean -xfd");
+      system( "git checkout -f " . $project->{rev} );
+
+      # Build
+
+      if ( -r "Configure.pl" )
+      {
+        system("perl Configure.pl --prefix=$install_dir");
+        system("make");
+        system("make install");
+      }
+      else
+      {
+        die "Could not build " . $project->{project} . "\n";
+      }
+
+    };
+
+    $build_it->($todo);
+    foreach my $child ( @{ $todo->{children} } )
+    {
+      $build_it->($child);
+    }
+
   }
 
+  # Remember what I said about a forever loop?  I lied.
+  last;
 }
