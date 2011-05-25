9a1252fe58ce19ea71492523a2a365a350a2eb75
Jon Gentle <atrodo@atrodo.org>
2011-05-25 07:56:56 -0400

    Run perltidy on the api module

--- lib/itfy/Controller/RPC/V1.pm
+++ lib/itfy/Controller/RPC/V1.pm
@@ -4,7 +4,7 @@ use namespace::autoclean;
 use JSON::Any;
 use autodie qw/:all/;
 
-BEGIN {extends 'Catalyst::Controller'; }
+BEGIN { extends 'Catalyst::Controller'; }
 
 =head1 NAME
 
@@ -18,28 +18,28 @@ Catalyst Controller.
 
 =cut
 
-
 =head2 default
 
 =cut
 
-sub default :Path :Args
+sub default : Path : Args
 {
-  my $self = shift;
-  my $c    = shift;
+  my $self    = shift;
+  my $c       = shift;
   my $api_key = shift;
 
-  my $machine = $c->model('ItfyDB::Machine')->find({ api_key => $api_key});
-  if (!defined $machine)
+  my $machine = $c->model('ItfyDB::Machine')->find( { api_key => $api_key } );
+  if ( !defined $machine )
   {
-    $c->response->status( 401);
-    $c->response->body(JSON::Any->objToJson( { error => "API Key is incorrect" } ) );
+    $c->response->status(401);
+    $c->response->body(
+      JSON::Any->objToJson( { error => "API Key is incorrect" } ) );
     $c->response->content_type("application/json");
     $c->detach();
   }
 
   $c->stash->{machine} = $machine;
-  $c->stash->{rpcout} = {};
+  $c->stash->{rpcout}  = {};
 
   $c->forward( join "/", @_ );
 
@@ -53,92 +53,101 @@ sub default :Path :Args
 
 sub revhash
 {
-  my $rev = shift;
+  my $rev  = shift;
   my $proj = $rev->bench_branch->project;
 
   my @children;
   my $children_rs = $rev->children;
-  while (my $child = $children_rs->next)
+  while ( my $child = $children_rs->next )
   {
     push @children, revhash($child);
   }
 
   my @cmds;
   my $cmd_rs = $proj->bench_cmds;
-  while (my $cmd = $cmd_rs->next)
+  while ( my $cmd = $cmd_rs->next )
   {
-    push @cmds, {
-      cmd => $cmd->cmd,
+    push @cmds,
+        {
+      cmd   => $cmd->cmd,
       count => $cmd->count,
-    }
+        };
   }
 
   return {
-    project => $proj->name,
+    project  => $proj->name,
     git_name => $proj->git_name,
-    url => $proj->url,
-    rev_id => $rev->bench_branch_rev_id,
-    rev => $rev->revision,
-    cmds => [@cmds],
+    url      => $proj->url,
+    rev_id   => $rev->bench_branch_rev_id,
+    rev      => $rev->revision,
+    cmds     => [@cmds],
     children => [@children],
   };
 }
 
-sub todo :Private
+sub todo : Private
 {
   my $self = shift;
   my $c    = shift;
 
   # Find all the missing revisions
-  my $done_rev_rs = $c->stash->{machine}->bench_runs->search({},
-  {
-    select => 'bench_branch_rev_id',
-    distinct => 1,
-  });
-  my $rev_rs = $c->model('ItfyDB::BenchBranchRev')->search({
-    'me.bench_branch_rev_id' => { '-not_in' => $done_rev_rs->as_query },
-    'me.parent_bench_branch_rev_id' => undef,
-  },{
-    order_by => "me.revision_stamp desc",
-    #prefetch => [qw/children/],
-  })->slice(0,5);
+  my $done_rev_rs = $c->stash->{machine}->bench_runs->search(
+    {},
+    {
+      select   => 'bench_branch_rev_id',
+      distinct => 1,
+    }
+  );
+  my $rev_rs = $c->model('ItfyDB::BenchBranchRev')->search(
+    {
+      'me.bench_branch_rev_id' => { '-not_in' => $done_rev_rs->as_query },
+      'me.parent_bench_branch_rev_id' => undef,
+    },
+    {
+      order_by => "me.revision_stamp desc",
+
+      #prefetch => [qw/children/],
+    }
+  )->slice( 0, 5 );
+
   #$rev_rs->result_class('DBIx::Class::ResultClass::HashRefInflator');
 
   # If they are missing any of the last 5 revisions, send the newest.
   # Otherwise, send a random old revision
 
   my @result;
-  while (my $rev = $rev_rs->next)
+  while ( my $rev = $rev_rs->next )
   {
     push @result, revhash($rev);
   }
   $c->stash->{rpcout}->{list} = [@result];
-  $c->log->debug(Data::Dumper::Dumper( $c->stash->{rpcout}->{list}));
+  $c->log->debug( Data::Dumper::Dumper( $c->stash->{rpcout}->{list} ) );
 }
 
 use Data::Dumper;
 
 sub get_rev_bydate
 {
-  my $c = shift;
+  my $c        = shift;
   my $git_name = shift;
-  my $date = shift;
+  my $date     = shift;
 
   local $ENV{GIT_DIR} = $c->config->{meta_repo_root} . "/$git_name/.git";
 
   system("git fetch");
 
-  my $rev = qx#git log --first-parent --before="$date" --format=%H origin/master | head -1#;
+  my $rev
+      = qx#git log --first-parent --before="$date" --format=%H origin/master | head -1#;
   chomp $rev;
 
-  return get_rev($c, $git_name, $rev);
+  return get_rev( $c, $git_name, $rev );
 }
 
 sub get_rev
 {
-  my $c = shift;
+  my $c        = shift;
   my $git_name = shift;
-  my $rev = shift;
+  my $rev      = shift;
 
   local $ENV{GIT_DIR} = $c->config->{meta_repo_root} . "/$git_name/.git";
 
@@ -148,62 +157,73 @@ sub get_rev
   my $git_log = qx/git log $rev -1 --format=%H:%ct:%cD/;
   chomp $git_log;
 
-  my ($rev_hash, $rev_stamp, $rev_date) = split m/:/, $git_log, 3;
+  my ( $rev_hash, $rev_stamp, $rev_date ) = split m/:/, $git_log, 3;
   my $git_describe = qx/git describe $rev --tags/;
   chomp $git_describe;
 
   return (
-    revision => $rev_hash,
-    revision_aka => $git_describe,
-    revision_date => $rev_date,
+    revision       => $rev_hash,
+    revision_aka   => $git_describe,
+    revision_date  => $rev_date,
     revision_stamp => $rev_stamp,
   );
 
 }
 
-sub add_branch_rev :Private
+sub add_branch_rev : Private
 {
   my $self = shift;
   my $c    = shift;
 
-  my $rpcin = JSON::Any->jsonToObj($c->request->params->{payload} || "{}");
+  my $rpcin = JSON::Any->jsonToObj( $c->request->params->{payload} || "{}" );
 
   # Find the project
-  my $project = $c->model('ItfyDB::Project')->find({
-    name => { like => $rpcin->{repository}->{name} },
-  });
+  my $project = $c->model('ItfyDB::Project')->find(
+    {
+      name => { like => $rpcin->{repository}->{name} },
+    }
+  );
   die "Cannot find project: " . $rpcin->{repository}->{name}
-    unless defined $project;
+      unless defined $project;
 
   my $git_name = $project->git_name;
 
   # Find the branch
   my $branch_name = $rpcin->{ref};
   $branch_name =~ s[refs/heads/][]xms;
-  my $branch = $project->bench_branch->find({
-    name => $branch_name,
-  });
+  my $branch = $project->bench_branch->find(
+    {
+      name => $branch_name,
+    }
+  );
 
   # Find the final, pushed revision
   my $rev = $rpcin->{after};
 
   # Save the revision
-  eval
-  {
-    my $new_rev = $branch->create_related("revs", {
-      get_rev($c, $git_name, $rev),
-    });
+  eval {
+    my $new_rev = $branch->create_related(
+      "revs",
+      {
+        get_rev( $c, $git_name, $rev ),
+      }
+    );
 
     my $dep_rs = $new_rev->bench_branch->dependencies;
-    while (my $dep = $dep_rs->next)
+    while ( my $dep = $dep_rs->next )
     {
       $dep = $dep->dep_bench_branch;
-      $new_rev->create_related("children",
-      {
-        bench_branch_id => $dep->bench_branch_id,
-        #parent_bench_branch_id => $new_rev->bench_branch_id,
-        get_rev_bydate($c, $dep->project->git_name, $new_rev->revision_date),
-      });
+      $new_rev->create_related(
+        "children",
+        {
+          bench_branch_id => $dep->bench_branch_id,
+
+          #parent_bench_branch_id => $new_rev->bench_branch_id,
+          get_rev_bydate(
+            $c, $dep->project->git_name, $new_rev->revision_date
+          ),
+        }
+      );
     }
   };
 }
