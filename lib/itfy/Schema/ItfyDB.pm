b6cee0865fff76c1c057fee6d319cd7aed01836c
Jon Gentle <atrodo@atrodo.org>
2011-05-25 07:53:38 -0400

    Update the ItfyDB schema to add dependencies from the meta file

--- lib/itfy/Schema/ItfyDB.pm
+++ lib/itfy/Schema/ItfyDB.pm
@@ -29,13 +29,47 @@ sub import_config
       my $bench_cmd = $bench_cmd_rs->update_or_create(%values);
     }
 
-    foreach my $branch_name (@{$project_c->{branch}})
+    foreach my $branch_c (@{$project_c->{branch}})
     {
       my $bench_branch_rs = $schema->resultset("BenchBranch");
       my $branch = $bench_branch_rs->update_or_create({
         project_id => $project->project_id,
-        name => $branch_name,
+        name => $branch_c->{name},
+        ref  => $branch_c->{ref},
       });
+    }
+  }
+
+  # Handle any project dependencies
+  foreach my $project_c (@{$config->{project}})
+  {
+    foreach my $branch_c (@{$project_c->{branch}})
+    {
+      if (exists $branch_c->{dependencies})
+      {
+        my $project = $schema->resultset("Project")->find({
+          name => $project_c->{name},
+        });
+        my $branch = $schema->resultset("BenchBranch")->find({
+          project_id => $project->project_id,
+          name => $branch_c->{name},
+        });
+        foreach my $dep (@{ $branch_c->{dependencies} })
+        {
+          my ($dep_project_name, $dep_branch_name) = split /:/, $dep;
+          my $project_dep = $schema->resultset("Project")->find({
+            name => $dep_project_name,
+          });
+          my $branch_dep = $schema->resultset("BenchBranch")->find({
+            project_id => $project_dep->project_id,
+            name => $dep_branch_name,
+          });
+          $schema->resultset("BenchBranchDep")->find_or_create({
+            bench_branch_id => $branch->bench_branch_id,
+            dep_bench_branch_id => $branch_dep->bench_branch_id,
+          });
+        }
+      }
     }
   }
 }
